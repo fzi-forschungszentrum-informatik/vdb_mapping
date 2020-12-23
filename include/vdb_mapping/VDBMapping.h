7a99baefe796cb314c397b000399b7e527eeed5f
Marvin Große Besselmann <grosse@fzi.de>
2020-12-23 12:42:30 +0100

    Initial skeleton commit

---
+++ include/vdb_mapping/VDBMapping.h
@@ -0,0 +1,13 @@
+#ifndef VDB_MAPPING_VDB_MAPPING_H_INCLUDED
+#define VDB_MAPPING_VDB_MAPPING_H_INCLUDED
+
+class VDBMapping
+{
+public:
+  VDBMapping();
+  virtual ~VDBMapping(){};
+private:
+  int a;
+};
+
+#endif /* VDB_MAPPING_VDB_MAPPING_H_INCLUDED */
---
+++ src/VDBMapping.cpp
@@ -0,0 +1,8 @@
+#include <vdb_mapping/VDBMapping.h>
+
+#include <iostream>
+
+VDBMapping::VDBMapping()
+{
+  std::cout << "hello vdb" << std::endl;
+}
