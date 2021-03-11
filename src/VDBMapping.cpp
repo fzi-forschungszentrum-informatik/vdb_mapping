33592ad2708c6181fb90d6241413747c30451e83
Marvin Große Besselmann <grosse@fzi.de>
2021-03-11 10:54:38 +0100

    Added license files

--- src/VDBMapping.cpp
+++ src/VDBMapping.cpp
@@ -1,3 +1,30 @@
+// this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
+
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+// Copyright 2021 FZI Forschungszentrum Informatik
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// -- END LICENSE BLOCK ------------------------------------------------
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Marvin Große Besselmann grosse@fzi.de
+ * \date    2020-12-23
+ *
+ */
+//----------------------------------------------------------------------
+
 #include <vdb_mapping/VDBMapping.h>
 
 #include <iostream>
