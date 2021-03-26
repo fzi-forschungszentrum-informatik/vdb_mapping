273c5a22def42d30ba80e9a24ef85c8ceee0459e
Marvin Große Besselmann <grosse@fzi.de>
2021-03-26 17:49:34 +0100

    Updated cmake configuration

--- vdb_mappingConfig.cmake
+++ vdb_mappingConfig.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" ${CMAKE_MODULE_PATH})
 
 include(CMakeFindDependencyMacro)
-
+find_dependency(Eigen3)
 find_dependency(OpenVDB)
-find_dependency(TBB)
 find_dependency(PCL)
+find_dependency(TBB)
 
 include("${CMAKE_CURRENT_LIST_DIR}/vdb_mappingTargets.cmake")
