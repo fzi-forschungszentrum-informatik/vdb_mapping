011c5f65e6eec68064d2edff46327b60435c9c4a
Marvin Große Besselmann <grosse@fzi.de>
2021-03-29 18:55:30 +0200

    removed TBB dependency

--- vdb_mappingConfig.cmake
+++ vdb_mappingConfig.cmake
@@ -4,6 +4,5 @@ include(CMakeFindDependencyMacro)
 find_dependency(Eigen3)
 find_dependency(OpenVDB)
 find_dependency(PCL)
-find_dependency(TBB)
 
 include("${CMAKE_CURRENT_LIST_DIR}/vdb_mappingTargets.cmake")
