d2202eee5da0ac576cfd33632765a29c278d06ad
Marvin Große Besselmann <grosse@fzi.de>
2023-11-21 13:57:07 +0100

    Changed to newer find script and modern cmake
    Signed-off-by: Marvin Große Besselmann <grosse@fzi.de>

--- vdb_mappingConfig.cmake
+++ vdb_mappingConfig.cmake
@@ -3,6 +3,7 @@ set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" ${CMAKE_MODULE_PATH})
 include(CMakeFindDependencyMacro)
 find_dependency(Eigen3)
 find_dependency(OpenVDB)
+find_dependency(Blosc)
 find_dependency(PCL)
 find_dependency(TBB)
 
